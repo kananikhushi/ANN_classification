b3efe98eb7509dc3bbc864a29fe5501e65313ecf
KHUSHI KANANI <127626625+kananikhushi@users.noreply.github.com>
2026-02-12 10:35:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = tf.keras.models.load_model('model.h5')
 
 #load encoders and scaler
 with open('onehot_encoder_geo.pkl', 'rb') as file:
-    label_encoder_geo = pickle.load(file)
+    onehot_encoder_geo = pickle.load(file)
 
 with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender=pickle.load(file)
@@ -20,7 +20,7 @@ with open('scaler.pkl', 'rb') as file:
 st.title('Customer Churn Prediction') 
 
 # Input fields
-geography = st.selectbox('Geography',label_encoder_geo.categories_[0])
+geography = st.selectbox('Geography',onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
@@ -45,8 +45,8 @@ input_data = pd.DataFrame({
 })
 
 # One-hot encode 'Geography'
-geo_encoded = label_encoder_geo.transform([[geography]]).toarray()
-geo_encoded_df = pd.DataFrame(geo_encoded, columns=label_encoder_geo.get_feature_names_out(['Geography']))
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
 # Combine one-hot encoded columns with input data
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
